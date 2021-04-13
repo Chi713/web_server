76e36fdfd2eeff786a4668fcfb8988bcf3eae0b7
Chi713 <chris.hoskin2355@gmail.com>
2021-04-13 19:41:57 -0400

    importing warp web server framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    warp::serve(warp::fs::dir("static"))
+        .run(([127, 0, 0, 1], 8000))
+        .await;
 }
